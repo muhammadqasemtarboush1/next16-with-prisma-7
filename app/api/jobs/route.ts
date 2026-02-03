5f2e569486815658abafba66e836262070ea322f
muhammadqasemtarboush1 <muhammad.tarboush1@gmail.com>
2026-02-03 03:35:43 +0300

    update: add job, mini dash, feach data

---
+++ app/api/jobs/route.ts
@@ -0,0 +1,44 @@
+import { auth } from "@/auth";
+import prisma from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const session = await auth();
+
+  if (!session?.user || !session.user.id) {
+    return NextResponse.redirect(new URL("/auth/singin", request.url));
+  }
+
+  try {
+    const data = await request.json();
+
+    const job = await prisma.job.create({
+      data: {
+        ...data,
+        postedById: session.user.id,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.error("Error creating job: ", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
+
+
+export async function GET(request: Request) {
+ 
+
+  try {
+    const jobs = await prisma.job.findMany({
+      orderBy: {
+        postedAt: "desc",
+      },
+    });
+    return NextResponse.json(jobs);
+  } catch (error) {
+    console.error("Error creating job: ", error);
+    return new NextResponse("Internal server error", { status: 500 });
+  }
+}
---
+++ auth.ts
@@ -0,0 +1,32 @@
+import NextAuth from "next-auth";
+import GitHub from "next-auth/providers/github";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "@/lib/prisma";
+// import { PrismaClient } from '@prisma/client'
+
+// use `prisma` in your application to read and write data in your DB
+// const prisma = new PrismaClient()
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  session: {
+    strategy: "jwt",
+  },
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+        token.name = user.name;
+      }
+
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+        session.user.name = token.name as string;
+      }
+      return session;
+    },
+  },
+});
